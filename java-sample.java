af6dc420de068d4345ecffc6c785ad2f5e994a47
Ny Hasinavalona Randriantsarafara <randriantsarafaranyhasinavalon@gmail.com>
2022-04-22 15:53:38 +0300

    chore: initial commit

---
+++ java-sample.java
@@ -0,0 +1,4 @@
+System.out.printlnt("Step 1");
+System.out.printlnt("Step 2");
+Thread.sleep(1000);
+System.out.printlnt("Step 3");
---
+++ nodejs-sample.js
@@ -0,0 +1,5 @@
+console.log("Step 1");
+setTimeout(() => {
+    console.log("Step 2");
+}, 1000);
+console.log("Step 3");
